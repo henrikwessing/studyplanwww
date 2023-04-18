381f3af0820ba49d0c1767eaa3722e728f39cd1f
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2023-04-18 23:02:26 +0200

    Update settings.py

--- cybertek/settings.py
+++ cybertek/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0z1@-*_$6jdejucokixm09$#^!6nf0$gck=4$6_)_xrs2z$zxi'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
